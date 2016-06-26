740ee367c4c90e3295fd301cb9c24d90367233d9
Sephrefet <aetherwoman@gmail.com>
2016-06-26 18:30:08 +0200

    RProgrammingAssignment2
    A repo for my second Assignment, as requested.

---
+++ cachematrix.R
@@ -0,0 +1,43 @@
+## catching the Inverse of a Matrix:
+## There may be some benefit to catching the inverse
+## of a matrix instead of computing it again and again.
+## I wrote a pair of functions that cache the inverse of
+## a matrix.
+
+## My function creates a matrix that can cache its 
+## inverse.
+
+makeCacheMatrix <- function(x = matrix()) {
+  inv <- NULL
+  set <- function(y){
+    x <<- y
+    inv <<- NULL
+  }
+get <- function()x
+setInverse <-function(inverse)inv <<-inverse
+getInverse <-function()inv
+list(set=set,get=get,
+     setInverse=setInverse,
+     getInverse=getInverse)
+}
+
+## My function computes the inverse of the special
+## "matrix" returned by makeCacheMatrix above. If 
+## the inverse has already been calculated (and the 
+## matrix has not changed), then cacheSolve should 
+## retrieve the inverse from the cache.
+
+
+cacheSolve <- function(x, ...) {
+  ## Return a matrix that is the inverse of 'x'
+  inv <- x$getInverse()
+  if (!is.null(inv)){
+    message("gettin cached data")
+    return(inv)
+  }
+  mat <-x$get()
+  inv<- solve(mat,...)
+  x$setInverse(inv)
+  inv
+}
+       
